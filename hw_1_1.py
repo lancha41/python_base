5d53f8ed1660d65019b42f94b0dcfc14a12ea3e6
lancha41 <lancha41@gmail.com>
2020-01-16 16:43:03 +0000

    I did first task

--- hw_1_1.py
+++ hw_1_1.py
@@ -1,1 +1,29 @@
+name_surname = input('Please, write your name and surname: ')
+print('='*20)
+print('Hello,', name_surname) 
+
+day_of_birth = int(input('Please, tell me the day of your birthday: '))
+month_of_birth = int(input('And month is: '))
+year_of_birth = int(input('At which year you was born? '))
+
+if (month_of_birth > 1) and (day_of_birth > 10):
+	years = 2020 - year_of_birth - 1
+	months = (years*12) +12 - month_of_birth
+	days =  months*30 - day_of_birth + 40
+elif (month_of_birth > 1) and (day_of_birth < 10):
+	years = 2020-year_of_birth - 1
+	months = years*12+ 13 - month_of_birth
+	days = months*30+10-day_of_birth
+elif (month_of_birth == 1) and (day_of_birth > 10):
+	years = 2020 - year_of_birth - 1
+	months = (years*12) + 12 - month_of_birth
+	days =  months*30 + 40 - day_of_birth
+elif (month_of_birth == 1) and (day_of_birth < 10):
+        years = 2020 - year_of_birth
+        months = (years*12)
+        days =  months*30 + 10 - day_of_birth
+
+print('-'*20)
+print('Wow! You have', years, 'years')
+print('And you live here for', months, 'months and', days, 'days!')
 
